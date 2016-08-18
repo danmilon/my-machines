d817f639242f86ee6923e20d280a2e6e2be89cf6
Dan Milon <i@danmilon.me>
2016-08-18 16:52:10 +0300

    6

---
+++ files/.emacs.d/python.el
@@ -0,0 +1,18 @@
+(add-hook 'python-mode-hook (lambda ()
+  (hack-local-variables)
+  (venv-workon project-venv-name)
+  (setq mode-line-format (cons '(:exec venv-current-name) mode-line-format))
+  (setq jedi:complete-on-dot t)
+  (setq jedi:get-in-function-call-delay 100)
+  (jedi:setup)
+  (electric-indent-mode -1)
+  (local-set-key (kbd "RET") 'newline-and-indent)))
+
+(defun pdb-unittest-current-file ()
+  "run pdb on current unittest file"
+  (interactive)
+  (let ((test-module
+    (file-name-sans-extension
+     (replace-regexp-in-string
+      "/" "." (substring (buffer-file-name) (+ (length project-root) 1))))))
+    (pdb (concat "nosetests -s --pdb --pdb-failures " test-module))))
