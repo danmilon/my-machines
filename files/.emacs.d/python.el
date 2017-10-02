25360200787749bd58330e67418ddd3e82988e13
Dan Milon <i@danmilon.me>
2017-10-02 11:03:40 +0300

    emacs: in elpy, swap flymake for flycheck

--- files/.emacs.d/python.el
+++ files/.emacs.d/python.el
@@ -1,5 +1,10 @@
 (elpy-enable)
 
+;; swap flymake for flycheck.
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
 (add-hook 'python-mode-hook (lambda ()
   (hack-local-variables)
   (ignore-errors
