1af4a1399bcbc5515ea03ba479a0709aec231ec0
Dan Milon <i@danmilon.me>
2019-06-10 15:30:59 +0300

    emacs: add plantuml

--- files/.emacs.d/org.el
+++ files/.emacs.d/org.el
@@ -6,3 +6,12 @@
 			     "~/Documents/org/life.org"))
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
+;; plantuml integration via org-babel
+;; https://eschulte.github.io/babel-dev/DONE-integrate-plantuml-support.html
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((plantuml . t)))
+
+(setq org-plantuml-jar-path
+      "/usr/share/java/plantuml/plantuml.jar")
