0636ac04afb04de559a1618084a5d99dd553e47a
Dan Milon <i@danmilon.me>
2020-02-20 17:22:31 +0200

    emacs: move org-mode to use-package

--- files/.emacs.d/org.el
+++ files/.emacs.d/org.el
@@ -1,17 +1,19 @@
-(require 'org)
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(setq org-log-done t)
-(setq org-agenda-files (list "~/Documents/org/work.org"
-			     "~/Documents/org/life.org"))
-(setq org-todo-keywords
-      '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+(use-package org
+  :bind (("C-c l" . org-store-link)
+	 ("C-c a" . org-agenda))
+  :config
+  (setq org-log-done t)
+  (setq org-agenda-files (list "~/Documents/org/work.org"
+			       "~/Documents/org/life.org"))
+  (setq org-todo-keywords
+	'((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
 
-;; plantuml integration via org-babel
-;; https://eschulte.github.io/babel-dev/DONE-integrate-plantuml-support.html
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((plantuml . t)))
+  ;; plantuml integration via org-babel
+  ;; https://eschulte.github.io/babel-dev/DONE-integrate-plantuml-support.html
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((plantuml . t)
+     (python . t)))
 
-(setq org-plantuml-jar-path
-      "/usr/share/java/plantuml/plantuml.jar")
+  (setq org-plantuml-jar-path
+	"/usr/share/java/plantuml/plantuml.jar"))
